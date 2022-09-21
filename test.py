b1d1c88983aefbd78ace8b72cf30692677276e57
akhra92 <akhrorrakhmonov@gmail.com>
2022-09-21 20:53:00 +0900

    yangi fayllar qushildi
    yangi fayllar

---
+++ test.py
@@ -0,0 +1,25 @@
+son = int(input("Istalgan butun son kiriting: "))
+for i in range(2,11):
+    if son % i == 0:
+        print(f"{son} soni {i} ga qoldiqsiz bulinadi") 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
